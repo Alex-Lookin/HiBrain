16aa91d3d724ce314bc7daf0b7f16b58b6179ca9
Aleks Lukin <al.lykin@mail.ru>
2024-10-30 01:23:50 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,44 @@
+// const arr = [1, 2, 3, 4, 5, 6 ];
+//     for(let i = arr.length; i>0; i--){
+//         console.log(arr[i-1])
+//     }
+// 'use strict'
+
+// const arr = [1, 2, 3, 4, 5, 6 ];
+//     for(let i = arr.length; i>0; i--){
+//         console.log(arr[i-1])
+//     }
+
+// const age = 40;
+//     if (age < 40) {
+//         console.log('Kaif')
+//     } else if (age === 40) {
+//         morgan = age **2
+//         console.log (morgan)
+//     } else {
+//         console.log('ne Kaif')
+//     }
+
+// const age = +prompt('Сколько тебе лет?')
+
+// switch (true) {
+//     case age < 1: {
+//         console.log('Такого возраста не бывает')
+//         break
+//     } 
+//     case age === 18: {
+//         console.log('Не верю, покажи паспорт.')
+//         break
+//     }
+//     case age > 0 && age <= 125: {
+//         console.log('Твой возраст: ${age}')
+//         break
+//     }
+//     case age > 125: {
+//         console.log('Вапир что ли?')
+//         break
+//     }
+//     default: {
+//         console.log('Возраст введен некорректно')
+//     }
+// }
